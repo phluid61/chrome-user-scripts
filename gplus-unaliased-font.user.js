78fb476772575a70f25d8282041e373e4238f2c4
Matthew Kerwin <matthew@kerwin.net.au>
2013-05-18 11:17:08 +1000

    added another class selector
    thinking of just breaking the Ruboto @font-face rule

--- gplus-unaliased-font.user.js
+++ gplus-unaliased-font.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://matthew.kerwin.net.au/
 // @description    Disables font aliasing for the new new Google+ layout.
 // @author         Matthew Kerwin
-// @version        1.0
+// @version        1.0.1
 // @include        http://plus.google.com/*
 // @include        https://plus.google.com/*
 // @include        http://*.plus.google.com/*
@@ -11,7 +11,7 @@
 // ==/UserScript==
 
 // Plain-text Message Body
-var css = 'body,.YF.a-n,.cw,.yd{font-family:"Helvetica Neue",Helvetica,Arial,sans-serif!important;}';
+var css = 'body,.YF.a-n,.cw,.yd,u0b{font-family:"Helvetica Neue",Helvetica,Arial,sans-serif!important;}';
 
 if (typeof addStyle != "undefined") {
     addStyle(css);
